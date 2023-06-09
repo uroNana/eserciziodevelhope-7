0a69913ba7dffa337d783f28f22a0487215be381
uroNana <mirianamartinelli97@libero.it>
2023-06-09 11:46:12 +0200

    exercise performed

---
+++ src/esercizio_7.java
@@ -0,0 +1,13 @@
+public class esercizio_7 {
+    public static void main(String[] args) {
+        String myName = "Miriana";
+
+        if (myName.length() % 2 == 0) {
+            System.out.print("the number of letters in your name is even");
+        } else {
+            System.out.print("the number of letters in your name is odd");
+        }
+
+
+    }
+}
